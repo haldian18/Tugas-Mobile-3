9a80d894a8cd3cd6352a36fddbc5b7a5211c38d3
HALDIAN <57083312+haldian18@users.noreply.github.com>
2022-09-28 11:05:31 +0700

    first commit

--- app/src/main/java/com/example/pertemuan3/MainActivity.java
+++ app/src/main/java/com/example/pertemuan3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,13 +18,21 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void panggil(View view) {
-        String nomor = "085768628231" ;
+        String nomor = "08xxxxxxxxx" ;
         Intent panggil = new Intent(Intent. ACTION_DIAL);
         panggil.setData(Uri. fromParts("tel",nomor,null));
         startActivity(panggil);
     }
+
+    public void share(View view){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(intent.EXTRA_TEXT, "Hai Saya Haldian Boleh Kenalan ???");
+        intent.setType("text/plain");
+        startActivity(Intent.createChooser(intent, "Share to :"));
+    }
+
     public void buka(View view) {
-        String url = "https://badoystudio.com" ;
+        String url = "https://youtube.com" ;
         Intent bukabrowser = new Intent(Intent. ACTION_VIEW);
         bukabrowser.setData(Uri. parse(url));
         startActivity(bukabrowser);
